a5645651eea198fe1c09afe9df08633e3abdf690
Viktor <viktoracri@gmail.com>
2020-04-15 09:48:13 +0200

    Version 1.0

---
+++ src/dev/acri/blockshuffle/BlockShuffle.java
@@ -0,0 +1,208 @@
+package dev.acri.blockshuffle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+
+public class BlockShuffle extends JavaPlugin{
+	
+	private static BlockShuffle instance;
+	
+	public BukkitTask runnable;
+	public boolean blockShuffleEnabled = false;
+	
+	private Random random = new Random();
+	
+	private long previousShuffle = -1;
+	private final int SHUFFLE_TIME_MINUTES = 5;
+	
+	public HashMap<Player, PlayerState> playersInGame = new HashMap<Player, PlayerState>();
+	
+	public List<Material> allowedMaterials = new ArrayList<Material>(Arrays.asList( new Material[]{
+			Material.COBBLESTONE,
+			Material.STONE,
+			Material.GRASS_BLOCK,
+			Material.DIRT,
+			Material.OAK_LOG,
+			Material.OAK_LEAVES,
+			Material.OAK_WOOD,
+			Material.SPRUCE_LOG,
+			Material.SPRUCE_LEAVES,
+			Material.SPRUCE_WOOD,
+			Material.BIRCH_LOG,
+			Material.BIRCH_LEAVES,
+			Material.BIRCH_WOOD,
+			Material.SNOW_BLOCK,
+			Material.SAND,
+			Material.STONE_BRICKS,
+			Material.CRACKED_STONE_BRICKS,
+			Material.BRICKS,
+			Material.MELON,
+			Material.OBSIDIAN,
+			Material.BOOKSHELF,
+			Material.SANDSTONE,
+			Material.COAL_BLOCK,
+			Material.SOUL_SAND,
+			Material.NETHER_BRICKS,
+			Material.NETHERRACK,
+			Material.GLOWSTONE,
+			Material.DIAMOND_ORE,
+			Material.GOLD_ORE,
+			Material.IRON_ORE,
+			Material.COAL_ORE,
+			Material.TNT,
+			Material.DISPENSER,
+			Material.NOTE_BLOCK,
+			Material.REDSTONE_BLOCK,
+			Material.HOPPER,
+			Material.COMPOSTER,
+			Material.BEDROCK,
+			Material.STONE_SLAB,
+			Material.COBBLESTONE_SLAB,
+			Material.STONE_BRICK_SLAB,
+			Material.GLASS,
+			Material.PISTON
+	}));
+	
+	
+	@Override
+	public void onEnable() {
+		BlockShuffle.instance = this;
+		this.getCommand("blockshuffle").setExecutor(new CommandBlockShuffle());
+		this.getCommand("blockshuffle").setTabCompleter(new CommandBlockShuffle());
+		for(Material mat : Material.values()) {
+			if(mat.name().startsWith("OAK"))
+				allowedMaterials.add(mat);
+			else if(mat.name().contains("DIORITE"))
+				allowedMaterials.add(mat);
+			else if(mat.name().contains("ANDESITE"))
+				allowedMaterials.add(mat);
+			else if(mat.name().contains("GRANITE"))
+				allowedMaterials.add(mat);
+		}
+
+		startRunnable();
+		
+		
+		
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+	
+	public void startRunnable() {
+		runnable = new BukkitRunnable() {
+			@Override
+			public void run() {
+				if(blockShuffleEnabled) {
+					if(System.currentTimeMillis() - previousShuffle > SHUFFLE_TIME_MINUTES * 60000){
+						// When a new round starts
+						previousShuffle = System.currentTimeMillis();
+						
+						Player failed = null;
+						for(Player all : playersInGame.keySet()) {
+							if(!playersInGame.get(all).hasFound) {
+								// If a player fails to find their block
+								Bukkit.broadcastMessage("§c§l" + all.getName() + " failed to find their block!");
+								failed = all;
+							}
+						}
+						
+						if(failed != null) playersInGame.remove(failed);
+						
+						if(playersInGame.size() == 1) {
+							blockShuffleEnabled = false;
+							for(Player p : playersInGame.keySet()) {
+								Bukkit.broadcastMessage("§a§l" + p.getName() + " won BlockShuffle!");
+								break;
+							}
+							return;
+						}
+						
+						
+						
+						
+						for(Player all : playersInGame.keySet()) {
+							Material mat = generateNewMaterial();
+							playersInGame.get(all).setMaterial(mat);
+							playersInGame.get(all).setFound(false);
+							all.sendMessage("§aYou need to stand on " + mat.name().toLowerCase().replaceAll("_", " "));
+							
+						}
+					}
+					
+					boolean found = true;
+					for(Player all : playersInGame.keySet()) {
+						if(!playersInGame.get(all).hasFound)
+							if(all.getLocation().add(0, -1, 0).getBlock().getType() == playersInGame.get(all).getMaterial()) {
+								// When their block is found
+								playersInGame.get(all).setFound(true);
+								Bukkit.broadcastMessage("§6§l" + all.getName() + " has found their block!");
+							}else
+								found = false;
+					}
+					
+					if(found) previousShuffle = -1;
+					
+					
+					
+				}
+			}}.runTaskTimer(this, 0, 10);
+	}
+	
+	public void startGame() {
+		playersInGame.clear();
+		for(Player all : Bukkit.getOnlinePlayers())
+			playersInGame.put(all, new PlayerState());
+		blockShuffleEnabled = true;
+		previousShuffle = -1;
+	}
+	
+	public Material generateNewMaterial() {
+		
+		
+		return allowedMaterials.get(random.nextInt(allowedMaterials.size() - 1));
+	}
+	
+	public static BlockShuffle getInstance() {
+		return BlockShuffle.instance;
+	}
+	
+	public class PlayerState{
+		Material material = null;
+		boolean hasFound = true;
+		
+		
+		public void setMaterial(Material mat) {
+			this.material = mat;
+		}
+		
+		public void setFound(boolean found) {
+			this.hasFound = found;
+		}
+		
+		public Material getMaterial() {
+			return this.material;
+		}
+		
+		public boolean hasFound() {
+			return this.hasFound;
+		}
+
+		
+		
+	}
+}
---
+++ src/dev/acri/blockshuffle/CommandBlockShuffle.java
@@ -0,0 +1,44 @@
+package dev.acri.blockshuffle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+
+import dev.acri.blockshuffle.BlockShuffle.PlayerState;
+
+public class CommandBlockShuffle implements TabCompleter, CommandExecutor{
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		if(!sender.hasPermission("blockshuffle.admin")) {
+			sender.sendMessage("§4You do not have permission.");
+			return true;
+		}
+		
+		if(BlockShuffle.getInstance().blockShuffleEnabled) {
+			BlockShuffle.getInstance().blockShuffleEnabled = false;
+			BlockShuffle.getInstance().playersInGame.clear();
+			Bukkit.broadcastMessage("§8[§6§lB§e§lS§8] §aBlockShuffle has been disabled");
+			
+		}else {
+			BlockShuffle.getInstance().startGame();
+			Bukkit.broadcastMessage("§8[§6§lB§e§lS§8] §aBlockShuffle has been enabled");
+		}
+		
+		return true;
+	}
+	
+	@Override
+	public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		
+		return new ArrayList<String>();
+	}
+}
